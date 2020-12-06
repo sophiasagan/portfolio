c277e0c412ab52a725601dbcb1bb8d67afb9e1ef
Sophia Braddock <sophiabraddock82@gmail.com>
2020-12-06 18:54:57 -0500

    projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,43 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
+import { FaAlignRight } from "react-icons/fa"
 
-export default () => {
-  return <Layout>
-    <Hero />
-    <Services />
-    <Jobs />
-  </Layout>
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+  return (
+    <Layout>
+      <Hero />
+      <Services />
+      <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
+    </Layout>
+  )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
