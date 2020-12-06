6fc4e713e2b771d70b19b8b73513f1d56afd79cb
Sophia Braddock <sophiabraddock82@gmail.com>
2020-12-06 16:58:11 -0500

    experience page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,40 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return <header className="hero">
+    <div className="section-center hero-center">
+      <article className="hero-info">
+        <div className="underline"></div>
+        <h1> I'm Sophia</h1>
+        <h4>full stack web developer</h4>
+        <Link to="/contact" className="btn">
+          contact me
+        </Link>
+        <SocialLinks />
+      </article>
+      <Image fluid={fluid} className="hero-img"/>
+
+    </div>
+    </header>
 }
 
 export default Hero
