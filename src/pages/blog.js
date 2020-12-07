19a792c3f1eea4895777660d00261f98a6d6bb02
Sophia Braddock <sophiabraddock82@gmail.com>
2020-12-07 16:02:58 -0500

    more seo

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,17 +2,21 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
+import SEO from "../components/SEO"
 
 const Blog = ({
   data: {
     allStrapiBlogs: { nodes: blogs },
   },
 }) => {
-  return <Layout>
-    <section className="blog-page">
-      <Blogs blogs={blogs} title="articles" />
-    </section>
-  </Layout>
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="articles" />
+      </section>
+    </Layout>
+  )
 }
 
 export const query = graphql`
